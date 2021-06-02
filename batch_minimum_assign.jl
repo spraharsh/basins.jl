beccacd4a7fbfe8bc6ea1e69f4351a5e5f0ee7c7
Praharsh Suryadevara <praharsharmm@gmail.com>
2021-06-01 23:28:57 -0500

    extra run comparisons

--- batch_minimum_assign.jl
+++ batch_minimum_assign.jl
@@ -10,15 +10,13 @@ include("minimumassign/minimumassign.jl")
 include("utils/utils.jl")
 
 
-
-
 # Generate radii
 
 
 
 
 natoms = 8
-radii_arr = generate_radii(0, 8, 1.,1.4, 0.05, 0.05*1.4)
+radii_arr = generate_radii(0, natoms, 1.,1.4, 0.05, 0.05*1.4)
 dim = 2.0
 phi = 0.9
 power = 2.5
@@ -26,7 +24,8 @@ eps = 1
 
 length = get_box_length(radii_arr, dim, phi)
 
-potential = InversePowerPeriodic(2, power, eps, [length, length],  radii_arr)
+potential_qndf = InversePowerPeriodic(2, power, eps, [length, length],  radii_arr)
+potential_cvode = InversePowerPeriodic(2, power, eps, [length, length],  radii_arr)
 
 
 
@@ -35,15 +34,31 @@ println(length)
 coords = generate_random_coordinates(length, natoms, dim)
 
 
+tol = 1e-9
+ba_qndf = BasinAssigner(QNDF(), tol, tol, 1e-6) # The convergence tolerance is error in gradient
+ba_cvode = BasinAssigner(CVODE_Adams(), tol, tol, 1e-6)
+ba_auto_switch = BasinAssigner(VCABM(), tol, tol, 1e-6)
 
-ba_qndf = BasinAssigner(QNDF(), 1e-4, 1e-4, 1e-6) # The convergence tolerance is error in gradient
-ba_cvode = BasinAssigner(CVODE_BDF(), 1e-4, 1e-4, 1e-6)
 
-# final_qndf= find_corresponding_minimum(ba_qndf, gradient_problem_function(potential), coords, 1000)
-final_cvode= find_corresponding_minimum(ba_cvode, gradient_problem_function(potential), coords, 1000)
+final_qndf = find_corresponding_minimum(ba_qndf, gradient_problem_function_qndf!(potential_qndf), coords, 10000)
 
-println(final)
+final_cvode = find_corresponding_minimum(ba_cvode, gradient_problem_function_cvode!(potential_cvode), coords, 10000)
+
+final_auto_switch = find_corresponding_minimum(ba_auto_switch, gradient_problem_function_qndf!(potential_qndf), coords, 10000)
+
+
+println(potential_cvode.f_eval)
+println(potential_cvode.jac_eval)
+println(final_qndf)
 println(final_cvode)
+
+
+println("Hessian CVODE")
+println(eigvals(system_hessian!(potential_cvode, final_qndf[1])))
+println("Final Hessian QNDF")
+println(eigvals(system_hessian!(potential_qndf, final_cvode[1])))
+
+
 
 
 
